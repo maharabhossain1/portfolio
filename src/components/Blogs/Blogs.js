e9e4a415cf792933e648994549946cbf28cbf6cd
maharabhossain1 <maharabhossain0022@gmail.com>
2022-06-20 15:19:48 +0600

    update

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Blogs() {
   return (
     <div className="m-5 p-5 text-center">
-      <h4> Comming Soon ...</h4>
+      <h4> Comming Soon ... okay </h4>
     </div>
   );
 }
